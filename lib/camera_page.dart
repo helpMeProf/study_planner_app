c471e685737af0c1157d2142e85d94c595390751
Dong Geun <dlehdrms978@gmail.com>
2021-09-26 21:13:47 +0900

    타이머 추가 완료

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -7,7 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_app/utils.dart';
 import 'package:google_ml_vision/google_ml_vision.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
-import 'package:image/image.dart' as imglib;
+
 import 'timer.dart';
 
 class Camera extends StatefulWidget{
@@ -21,8 +21,10 @@ class _CameraState extends State<Camera>{
   late CameraController controller;
   late CameraDescription camera;
   late FaceDetector faceDetector;
-  late final interpreter;
 
+  late final interpreter;
+  int closedCount=0;
+  Icon icon = Icon(Icons.play_arrow);
 
   bool isDetecting = false;
   @override
@@ -48,6 +50,7 @@ class _CameraState extends State<Camera>{
     controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -64,10 +67,22 @@ class _CameraState extends State<Camera>{
                initialChildSize: 1.0,
                builder: (context,scrollController){
                  return SingleChildScrollView(
-                   child: Container(
-                       width: controller.value.previewSize?.width,
-                       height: controller.value.previewSize?.height,
-                       child: TimerWidget(controller: controller,interpreter: interpreter, faceDetector: faceDetector,camera: camera)
+                   padding: const EdgeInsets.all(20),
+                   child: Column(
+                     children: [
+                       SizedBox(
+                         child: AspectRatio(
+                             aspectRatio: controller.value.aspectRatio,
+                             child: CameraPreview(controller),
+                             )
+                         ),
+                       SizedBox(
+                           child:AspectRatio(
+                             aspectRatio: controller.value.aspectRatio,
+                             child: TimerWidget(faceDetector: faceDetector,camera: camera,controller: controller,interpreter: interpreter),
+                           )
+                       )
+                     ],
                    )
                  );
                },
@@ -77,47 +92,10 @@ class _CameraState extends State<Camera>{
            }
          },
         ),
-      floatingActionButton: FloatingActionButton(onPressed: () async {
+      floatingActionButton: FloatingActionButton(
+        onPressed:() {
 
-        if(controller.value.isStreamingImages) return;
-          controller.startImageStream((image) {
-
-          if(!isDetecting){
-            isDetecting = true;
-            ImageRotation rotation = rotationIntToImageRotation(camera.sensorOrientation);
-            var metadata = buildMetaData(image,rotation);
-            var unit8list = planesToUnit8(image.planes);
-            try {
-              final GoogleVisionImage visionImage = GoogleVisionImage
-                  .fromBytes(unit8list, metadata);
-                faceDetector.processImage(visionImage).then((List<Face> result) {
-                isDetecting = false;
-                if(result.isEmpty) {print("not found"); return null;}
-                print(result[0].getContour(FaceContourType.leftEye)?.positionsList.toString());
-
-                List<Offset>? leftEyesPosition = result[0].getContour(FaceContourType.leftEye)?.positionsList;
-                List<Offset>? rightEyesPosition = result[0].getContour(FaceContourType.rightEye)?.positionsList;
-
-                imglib.Image img = convertYUV420(image);
-
-                imglib.Image left_eyes = imglib.copyResize(crop_eye(img, leftEyesPosition!),width: 34,height: 26);
-                imglib.Image right_eyes = imglib.copyResize(crop_eye(img,rightEyesPosition!),width: 34,height: 26);
-
-                var left_eye_gray = grayscaleToByteListFloat32(left_eyes);
-                var right_eye_gray = grayscaleToByteListFloat32(right_eyes);
-
-                var left_eye_output = List.filled(1,0).reshape([1,1]);
-                var right_eye_output = List.filled(1,0).reshape([1,1]);
-
-                interpreter.run(left_eye_gray,left_eye_output);
-                interpreter.run(right_eye_gray,right_eye_output);
-              });
-            }catch(e){
-              print("error발생");
-            }
-          }
-        });
-      },
+        },
         child: Icon(Icons.play_arrow),
       ),
     );
--- lib/timer.dart
+++ lib/timer.dart
@@ -1,31 +1,175 @@
+import 'dart:async';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/utils.dart';
 import 'package:google_ml_vision/google_ml_vision.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:image/image.dart' as imglib;
 class TimerWidget extends StatefulWidget {
   final CameraController controller;
+  final CameraDescription camera;
   final Interpreter interpreter;
   final FaceDetector faceDetector;
-  final CameraDescription camera;
-  const TimerWidget({Key? key, required this.controller,required this.interpreter,required this.faceDetector,required this.camera}) : super(key: key);
+  const TimerWidget({Key? key, required this.controller, required this.camera, required this.interpreter, required this.faceDetector}) : super(key: key);
 
 
   @override
-  State<TimerWidget> createState() => _TimerWidgetState();
+  State<TimerWidget> createState() => TimerWidgetState();
+
 }
 
-class _TimerWidgetState extends State<TimerWidget> {
-  bool isDetecting = false;
+class TimerWidgetState extends State<TimerWidget> {
+  @override
+  void dispose(){
+    print("hh");
+    super.dispose();
+  }
 
+  late Timer _timer;
+  var _time=0;
+  var _icon = Icons.play_arrow;
+  bool _isPlaying = false;
+
+  bool _isDetecting = false;
+  int _closedCount = 0;
+  
+  void click(){
+    _isPlaying = !_isPlaying;
+    if(_isPlaying){
+      _icon = Icons.pause;
+      _start();
+    }else{
+      _icon = Icons.play_arrow;
+      _pause();
+    }
+  }
+  void _start(){
+    startDetecting();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+  void _pause(){
+    stopDetecting();
+    _timer.cancel();
+    setState(() {});
+  }
+  stopDetecting(){
+    if(widget.controller.value.isStreamingImages) {
+      widget.controller.stopImageStream();
+    }
+  }
+  startDetecting() {
+
+    if(widget.controller.value.isStreamingImages) return;
+    _closedCount=0;
+    widget.controller.startImageStream((image) {
+      if(!_isDetecting){
+        _isDetecting = true;
+        ImageRotation rotation = rotationIntToImageRotation(widget.camera.sensorOrientation);
+        var metadata = buildMetaData(image,rotation);
+        var unit8list = planesToUnit8(image.planes);
+        try {
+          final GoogleVisionImage visionImage = GoogleVisionImage
+              .fromBytes(unit8list, metadata);
+          widget.faceDetector.processImage(visionImage).then((List<Face> result) {
+            _isDetecting = false;
+            if(result.isEmpty) {print("not found"); return null;}
+            print(result[0].getContour(FaceContourType.leftEye)?.positionsList.toString());
+
+            List<Offset>? leftEyesPosition = result[0].getContour(FaceContourType.leftEye)?.positionsList;
+            List<Offset>? rightEyesPosition = result[0].getContour(FaceContourType.rightEye)?.positionsList;
+
+            imglib.Image img = convertYUV420(image);
+
+            imglib.Image left_eyes = imglib.copyResize(crop_eye(img, leftEyesPosition!),width: 34,height: 26);
+            imglib.Image right_eyes = imglib.copyResize(crop_eye(img,rightEyesPosition!),width: 34,height: 26);
+
+            var left_eye_gray = grayscaleToByteListFloat32(left_eyes);
+            var right_eye_gray = grayscaleToByteListFloat32(right_eyes);
+
+            var left_eye_output = List.filled(1,0).reshape([1,1]);
+            var right_eye_output = List.filled(1,0).reshape([1,1]);
+
+            widget.interpreter.run(left_eye_gray,left_eye_output);
+            widget.interpreter.run(right_eye_gray,right_eye_output);
+
+            var left_open = left_eye_output[0][0];
+            var right_open = right_eye_output[0][0];
+            print(left_open);
+            print(right_open);
+            print(_closedCount);
+            //0.4~0.5초당 1번 검사 하고 10초에 28번
+            //넉넉 잡아 10초동안 눈을 뜨지 않았을 경우는 30번 카운팅
+
+            if(left_open<0.4&&right_open<0.4){
+              _closedCount++;
+            }else {
+              _closedCount=0;
+            }
+            if(_closedCount>=30){
+              if(widget.controller.value.isStreamingImages) {
+                widget.controller.stopImageStream();
+              }
+            }
+          });
+        }catch(e){
+          print("error발생");
+        }
+
+      }
+    });
+  }
+  Widget _body(){
+    var hour = (_time~/3600)<10?'0${(_time~/3600)}':(_time~/3600);
+    var min = (_time%3600)~/60<10?'0${(_time%3600)~/60}':(_time%3600)~/60;
+    var sec = (_time%3600)%60<10?'0${(_time%3600)%60}':(_time%3600)%60;
+    return Center(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text('$hour',style: TextStyle(fontSize: 80)),
+              Text(':$min',style: TextStyle(fontSize: 80)),
+              Text(':$sec',style: TextStyle(fontSize: 80)),
+            ],
+
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children : [
+                IconButton(
+                  padding: EdgeInsets.all(10),
+                  onPressed: () {
+                    click();
+                  },
+                  icon: Icon(_icon,size: 50,),
+                ),
+                IconButton(
+                  padding: EdgeInsets.all(10),
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                  },
+                  icon: Icon(Icons.stop,size: 50)
+                )
+              ]
+          )
+        ],
+      ),
+
+    );
+  }
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: AspectRatio(
-        aspectRatio: widget.controller.value.aspectRatio,
-        child: CameraPreview(widget.controller),
-      ),
+      body: _body()
     );
   }
 }
